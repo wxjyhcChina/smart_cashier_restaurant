f010b7b0fd7fbdf3a9653f743788f90778b05e21
Binqi Yuan <binqi.yuan@boosal.com>
2019-06-05 10:27:47 +0800

    update link

--- resources/views/backend/includes/footer.blade.php
+++ resources/views/backend/includes/footer.blade.php
@@ -2,7 +2,7 @@
 <footer class="main-footer">
     <!-- To the right -->
     <div class="pull-right hidden-xs">
-        <a href="http://laravel-boilerplate.com" target="_blank">{{ app_name() }}</a>
+        <a href="http://www.jyhongchuang.com" target="_blank">{{ app_name() }}</a>
     </div>
     <!-- Default to the left -->
     <strong>Copyright &copy; {{ date('Y') }} <a href="#">{{ company_name() }}</a>.</strong> {{ trans('strings.backend.general.all_rights_reserved') }}
