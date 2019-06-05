bc2dfe0cb3b29d09beaf3f7d2a4443cdd884c5e1
Binqi Yuan <binqi.yuan@boosal.com>
2019-06-05 10:32:53 +0800

    remove useless ui

--- resources/views/backend/includes/header.blade.php
+++ resources/views/backend/includes/header.blade.php
@@ -29,47 +29,47 @@
                     </li>
                 @endif
 
-                <li class="dropdown messages-menu">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <i class="fa fa-envelope-o"></i>
-                        <span class="label label-default">0</span>
-                    </a>
+{{--                <li class="dropdown messages-menu">--}}
+{{--                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">--}}
+{{--                        <i class="fa fa-envelope-o"></i>--}}
+{{--                        <span class="label label-default">0</span>--}}
+{{--                    </a>--}}
 
-                    <ul class="dropdown-menu">
-                        <li class="header">{{ trans_choice('strings.backend.general.you_have.messages', 0, ['number' => 0]) }}</li>
-                        <li class="footer">
-                            {{ link_to('#', trans('strings.backend.general.see_all.messages')) }}
-                        </li>
-                    </ul>
-                </li><!-- /.messages-menu -->
+{{--                    <ul class="dropdown-menu">--}}
+{{--                        <li class="header">{{ trans_choice('strings.backend.general.you_have.messages', 0, ['number' => 0]) }}</li>--}}
+{{--                        <li class="footer">--}}
+{{--                            {{ link_to('#', trans('strings.backend.general.see_all.messages')) }}--}}
+{{--                        </li>--}}
+{{--                    </ul>--}}
+{{--                </li><!-- /.messages-menu -->--}}
 
-                <li class="dropdown notifications-menu">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <i class="fa fa-bell-o"></i>
-                        <span class="label label-default">0</span>
-                    </a>
+{{--                <li class="dropdown notifications-menu">--}}
+{{--                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">--}}
+{{--                        <i class="fa fa-bell-o"></i>--}}
+{{--                        <span class="label label-default">0</span>--}}
+{{--                    </a>--}}
 
-                    <ul class="dropdown-menu">
-                        <li class="header">{{ trans_choice('strings.backend.general.you_have.notifications', 0) }}</li>
-                        <li class="footer">
-                            {{ link_to('#', trans('strings.backend.general.see_all.notifications')) }}
-                        </li>
-                    </ul>
-                </li><!-- /.notifications-menu -->
+{{--                    <ul class="dropdown-menu">--}}
+{{--                        <li class="header">{{ trans_choice('strings.backend.general.you_have.notifications', 0) }}</li>--}}
+{{--                        <li class="footer">--}}
+{{--                            {{ link_to('#', trans('strings.backend.general.see_all.notifications')) }}--}}
+{{--                        </li>--}}
+{{--                    </ul>--}}
+{{--                </li><!-- /.notifications-menu -->--}}
 
-                <li class="dropdown tasks-menu">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <i class="fa fa-flag-o"></i>
-                        <span class="label label-default">0</span>
-                    </a>
+{{--                <li class="dropdown tasks-menu">--}}
+{{--                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">--}}
+{{--                        <i class="fa fa-flag-o"></i>--}}
+{{--                        <span class="label label-default">0</span>--}}
+{{--                    </a>--}}
 
-                    <ul class="dropdown-menu">
-                        <li class="header">{{ trans_choice('strings.backend.general.you_have.tasks', 0, ['number' => 0]) }}</li>
-                        <li class="footer">
-                            {{ link_to('#', trans('strings.backend.general.see_all.tasks')) }}
-                        </li>
-                    </ul>
-                </li><!-- /.tasks-menu -->
+{{--                    <ul class="dropdown-menu">--}}
+{{--                        <li class="header">{{ trans_choice('strings.backend.general.you_have.tasks', 0, ['number' => 0]) }}</li>--}}
+{{--                        <li class="footer">--}}
+{{--                            {{ link_to('#', trans('strings.backend.general.see_all.tasks')) }}--}}
+{{--                        </li>--}}
+{{--                    </ul>--}}
+{{--                </li><!-- /.tasks-menu -->--}}
 
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
